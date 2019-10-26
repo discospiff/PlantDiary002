9758485bd88b2160f1a4b43c1cf3eb87f5d4cb15
jonesbr <brandan.jones@uc.edu>
2019-10-26 12:14:00 -0400

    Consume the JSON Specimens

--- PlantDiary002/Pages/Index.cshtml.cs
+++ PlantDiary002/Pages/Index.cshtml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -23,6 +24,25 @@ namespace PlantDiary002.Pages
             int age = 31;
             ViewData["MyName"] = myName;
             ViewData["age"] = age;
+
+            // download the JSON data.
+            // a web client gives us access to data on the internet.
+            using(WebClient webClient = new WebClient())
+            {
+                // get the raw JSON data.
+                string jsonData = webClient.DownloadString("https://www.plantplaces.com/perl/mobile/viewspecimenlocations.pl?Lat=39.14455075&Lng=-84.5093939666667&Range=0.5&Source=location&Version=2");
+                // Marshall the data into a series of objects.
+                QuickType.Welcome welcome = QuickType.Welcome.FromJson(jsonData);
+                // get the list (collection) of specimens
+                List<QuickType.Specimen> allSpecimens = welcome.Specimens;
+                // iterate over the specimens so we can shake hands with them.
+                foreach(QuickType.Specimen specimen in allSpecimens)
+                {
+                    // shake hands with one specimen at a time.
+                    Console.WriteLine(specimen);
+                }
+            }
+
 
         }
     }
