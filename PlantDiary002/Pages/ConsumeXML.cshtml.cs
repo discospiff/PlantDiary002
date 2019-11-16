1d2ef75dd96480c25ce1bca713d0481d49db8e38
jonesbr <brandan.jones@uc.edu>
2019-11-16 12:44:42 -0500

    First try at validation.

--- PlantDiary002/Pages/ConsumeXML.cshtml.cs
+++ PlantDiary002/Pages/ConsumeXML.cshtml.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Xml;
+using System.Xml.Schema;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -14,6 +15,7 @@ namespace PlantDiary002.Pages
     public class ConsumeXMLModel : PageModel
     {
         private IHostingEnvironment _environment;
+        private string result = "";
         public ConsumeXMLModel(IHostingEnvironment environment)
         {
             _environment = environment;
@@ -37,8 +39,37 @@ namespace PlantDiary002.Pages
             XmlDocument doc = new XmlDocument();
             doc.Load(file);
             XmlNode node = doc.SelectSingleNode("/plant/specimens/specimen[latitude>0]");
-            int foo = 1 + 1;
+            ValidateXML(file);
 
+        }
+
+        private void ValidateXML(string file)
+        {
+            // delcare our validation preferences
+            XmlReaderSettings settings = new XmlReaderSettings();
+            string xsdPath = Path.Combine(_environment.ContentRootPath, "uploads", "plants.xsd");
+            settings.Schemas.Add(null, xsdPath);
+
+            // validate with XSD
+            settings.ValidationType = ValidationType.Schema;
+
+            // a couple more flags.
+            settings.ValidationFlags |= System.Xml.Schema.XmlSchemaValidationFlags.ProcessSchemaLocation;
+            settings.ValidationFlags |= System.Xml.Schema.XmlSchemaValidationFlags.ReportValidationWarnings;
+
+            // who do we call when things go wrong?
+            settings.ValidationEventHandler += new System.Xml.Schema.ValidationEventHandler(this.ValidationEventHandler);
+
+            XmlReader xmlReader = XmlReader.Create(file, settings);
+            while (xmlReader.Read())
+            {
+
+            }
+        }
+
+        private void ValidationEventHandler(object sender, ValidationEventArgs args)
+        {
+            result = "validation failed.  Message: " + args.Message;
         }
     }
 }
