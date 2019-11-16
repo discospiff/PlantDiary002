1fc81afdbd54300dee5825bb245348bbc21b72f2
jonesbr <brandan.jones@uc.edu>
2019-11-16 12:18:55 -0500

    Save an uploaded document, consume it as DOM

---
+++ PlantDiary002/Pages/ConsumeXML.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PlantDiary002.Pages
+{
+    public class ConsumeXMLModel : PageModel
+    {
+        private IHostingEnvironment _environment;
+        public ConsumeXMLModel(IHostingEnvironment environment)
+        {
+            _environment = environment;
+        }
+
+        [BindProperty]
+        public IFormFile Upload { get; set; }
+        public void OnGet()
+        {
+
+        }
+
+        public void OnPost()
+        {
+            string fileName = Upload.FileName;
+            string file = Path.Combine(_environment.ContentRootPath, "uploads", fileName);
+            using (var fileStream = new FileStream(file, FileMode.Create))
+            {
+                Upload.CopyTo(fileStream);
+            }
+            XmlDocument doc = new XmlDocument();
+            doc.Load(file);
+            XmlNode node = doc.SelectSingleNode("/plant/specimens/specimen[latitude>0]");
+            int foo = 1 + 1;
+
+        }
+    }
+}
